62bbf83fcc2edc93972221b5e8f6410e19a50595
Ilshat <gubaidullin.i@gmail.com>
2019-12-03 11:17:11 +0300

    demod

--- ReceivingStation/Demodulator/Demodulating.cs
+++ ReceivingStation/Demodulator/Demodulating.cs
@@ -13,7 +13,7 @@ using ReceivingStation.Other;
 
 namespace ReceivingStation.Demodulator
 {
-    unsafe class Demodulating
+    unsafe class Demodulating ////fvgdfgdfgdfgdfgdfg
     {
        
         private uint _SampleRate;
